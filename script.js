e2624cdefbc39e137427c021e48b394f27545bc7
nanawaremayank20-404 <nanawaremayank20@gmail.com>
2026-03-09 17:48:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-function validateForm()
-{
+function saveToExcel(event){
+
+event.preventDefault();
 
 let name=document.getElementById("name").value;
 let email=document.getElementById("email").value;
+let phone=document.getElementById("phone").value;
+let country=document.getElementById("country").value;
+let date=document.getElementById("date").value;
+let message=document.getElementById("message").value;
 
-if(name=="" || email=="")
-{
-alert("Please fill all required fields");
-return false;
+let data="Name,Email,Phone,Country,Travel Date,Message\n";
+
+data+=name+","+email+","+phone+","+country+","+date+","+message;
+
+let blob=new Blob([data],{type:"text/csv"});
+
+let url=window.URL.createObjectURL(blob);
+
+let a=document.createElement("a");
+
+a.setAttribute("href",url);
+a.setAttribute("download","tourist_enquiry.csv");
+
+a.click();
+
+alert("Data saved. Excel file downloaded.");
 }
 
-alert("Enquiry submitted successfully!");
-
-return true;
-
-}
